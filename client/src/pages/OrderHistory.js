3adee14cff95b007074e78dc94dbd96c2670c26b
Kevin Yong <euler004@gmail.com>
2022-11-12 00:23:45 -0500

    removed imgs from order page

--- client/src/pages/OrderHistory.js
+++ client/src/pages/OrderHistory.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "./OrderHistory.css";	
-import shoe from "../images/shoe.png";
 
 export default function orders() {
 	const styleObj = {
@@ -16,6 +15,7 @@ export default function orders() {
 			<div className="description"> 
 			<div style={{ fontSize:33,paddingBottom: "10px"}}> Order History</div>
 			<div style={{paddingBottom: "20px"}}> Review your order history here.</div>
+			
 			<input type="text" placeholder="Search all orders"></input>
 			</div>
 			
@@ -25,10 +25,10 @@ export default function orders() {
 			<strong>Item:</strong> Blue White shoes<br />
 			<strong>Order ID:</strong> 123-456-7890<br />
 			<strong>Total:</strong> $123.45 <br />
-			<strong>Purchase Date:</strong> 12-31-2022 <br /><br />
+			<strong>Purchase Date:</strong> 12-31-2022 <br />
 			<strong>Seller:</strong> bob
 			</div>
-			<img src={shoe} alt="shoe" width="224px"/>
+	
 			<button type="button">Contact Support</button>
 			<button type="button" >Track Package</button>
 			<button type="button" >Leave the seller feedback</button>
@@ -40,10 +40,10 @@ export default function orders() {
 			<strong>Item:</strong> Blue White shoes<br />
 			<strong>Order ID:</strong> 123-456-7890<br />
 			<strong>Total:</strong> $123.45 <br />
-			<strong>Purchase Date:</strong> 12-31-2022 <br /><br />
+			<strong>Purchase Date:</strong> 12-31-2022 <br />
 			<strong>Seller:</strong> bob
 			</div>
-			<img src={shoe} alt="shoe" width="224px"/>
+		
 			<button type="button">Contact Support</button>
 			<button type="button" >Track Package</button>
 			<button type="button" >Leave the seller feedback</button>
@@ -55,25 +55,10 @@ export default function orders() {
 			<strong>Item:</strong> Blue White shoes<br />
 			<strong>Order ID:</strong> 123-456-7890<br />
 			<strong>Total:</strong> $123.45 <br />
-			<strong>Purchase Date:</strong> 12-31-2022 <br /><br />
+			<strong>Purchase Date:</strong> 12-31-2022 <br />
 			<strong>Seller:</strong> bob
-			</div>
-			<img src={shoe} alt="shoe" width="224px"/>
-			<button type="button">Contact Support</button>
-			<button type="button" >Track Package</button>
-			<button type="button" >Leave the seller feedback</button>
-			<button type="button" >Send a message to the seller</button>
 			</div>
 
-			<div className="order"> 
-			<div className="orderDetails"> 
-			<strong>Item:</strong> Blue White shoes<br />
-			<strong>Order ID:</strong> 123-456-7890<br />
-			<strong>Total:</strong> $123.45 <br />
-			<strong>Purchase Date:</strong> 12-31-2022 <br /><br />
-			<strong>Seller:</strong> bob
-			</div>
-			<img src={shoe} alt="shoe" width="224px"/>
 			<button type="button">Contact Support</button>
 			<button type="button" >Track Package</button>
 			<button type="button" >Leave the seller feedback</button>
